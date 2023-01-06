ef576b7c7106e65eec412565f75f0df9bd2c2499
azizamohammed <azizakhamis975@gmail.com>
2023-01-06 21:03:50 +0300

    first work

---
+++ MyOwnAutoShop.java
@@ -0,0 +1,16 @@
+*,*+++++++L:*l99i;//''0/image.pngimage.pngzz
+   /* public static void main(String[] args){
+
+        +Sedan Sedaan = new Sedan(160, 20000, "Blue", 20);
+        Ford  fod = new Ford (156,4452.0,"Black",2015, 30);
+        Ford fd = new Ford (155,5000.0,"white",2001, 5);
+        Car cr = new Car (555, 56856.0, "Red"); 
+
+        System.out.println("Sedan Sale Price" + Sedaan.getSalePrice());
+        System.out.println("Ford 1 Sale Price" + fod.getSalePrice());
+        System.out.println("Ford 2 Sale Price" + fd.getSalePrice());
+        System.out.println("Car Sale Price:  " + cr.getSalePrice());
+ }
+
+} ppppp                                                                                                      
++ 
